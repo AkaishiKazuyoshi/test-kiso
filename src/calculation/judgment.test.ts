cbbd69027f08d1fd28a2cdef12177a44fca8522d
Kazuyoshi Akaishi <k.akaishi@kazuyoshi_akaishi@mail.toyota.co.jp>
2023-11-18 14:38:38 +0900

    isEven, getGreaterValue, getGreatestValue

--- src/calculation/judgment.test.ts
+++ src/calculation/judgment.test.ts
@@ -1,8 +1,25 @@
-import {isOdd} from "./judgment";
+import {getGreaterValue, getGreatestValue, isEven, isOdd} from "./judgment";
 
 describe('judgement', () => {
   it('isOdd', () => {
     expect(isOdd(1)).toBeTruthy()
     expect(isOdd(2)).toBeFalsy()
+  })
+
+  it('isEven', () => {
+    expect(isEven(1)).toBeFalsy()
+    expect(isEven(2)).toBeTruthy()
+  });
+
+  it('getGreaterValue', () => {
+    expect(getGreaterValue(1, 2)).toBe(2)
+    expect(getGreaterValue(2, 1)).toBe(2)
+  })
+
+  it('getGreatestValue', () => {
+    expect(getGreatestValue(10, 15)).toBe(15)
+    expect(getGreatestValue(15, 10)).toBe(15)
+    expect(getGreatestValue(1, 2, 3, 4, 5)).toBe(5)
+    expect(getGreatestValue(1, 10, 3, 4, 5)).toBe(10)
   })
 })
--- src/calculation/judgment.ts
+++ src/calculation/judgment.ts
@@ -1,3 +1,17 @@
-export const isOdd = (num: number): boolean => {
-  return num % 2 === 1
+// 基礎コース1日目
+export const isOdd = (number: number): boolean => {
+  return number % 2 === 1
+}
+// 基礎コース3日目　％ 演算子を使用しないで isEven 関数を書き換えてください。
+export function isEven(number: number): boolean {
+  return number - (2 * Math.floor(number / 2)) === 0
+}
+// 基礎コース3日目　2 つの引数のうち大きい数字を返す getGreaterValue という名前の関数を作成してください。
+export const getGreaterValue = (a: number, b: number): number => {
+  return a > b ? a : b
+}
+// 基礎コース3日目　与えられた引数のうち一番大きい数字を返すgetGreatestValue という名前の関数を作成してください。
+export const getGreatestValue = (...numbers: number[]): number => {
+  // return numbers.reduce((a, b) => a > b ? a : b)
+  return Math.max(...numbers)
 }
