3cc50864e2facffe1170552e904a83f147d0c282
Kazuyoshi Akaishi <k.akaishi@kazuyoshi_akaishi@mail.toyota.co.jp>
2024-02-14 13:48:20 +0900

    searchObject, isOrder

--- src/calculation/calculation.test.ts
+++ src/calculation/calculation.test.ts
@@ -1,4 +1,4 @@
-import {calculation} from './calculation';
+import {calculation, inOrder, searchObject} from './calculation';
 
 describe('calculation', () => {
   describe('sum', () => {
@@ -6,4 +6,46 @@ describe('calculation', () => {
       expect(calculation(1, 2)).toBe(3);
     });
   });
+
+  describe('search object', () => {
+    it('search one words', () => {
+      const obj = {a: 1, b: {c: 2}};
+      expect(searchObject(obj, (value) => value === 1)).toEqual({'object.a': 1});
+    });
+    it('search some words', () => {
+      const obj = {a: 1, b: {c: 2, d: [{e: 3, f: 4}]}}
+      expect(searchObject(obj, (value) => value > 1)).toEqual({
+        'object.b.c': 2,
+        'object.b.d[0].e': 3,
+        'object.b.d[0].f': 4
+      });
+    });
+    it('search some words 2', () => {
+      const obj = {a: 1, b: {c: 2, d: [{e: 3}, {f: 4}]}}
+      expect(searchObject(obj, (value) => value > 1)).toEqual({
+        'object.b.c': 2,
+        'object.b.d[0].e': 3,
+        'object.b.d[1].f': 4
+      });
+    });
+  });
+
+  describe('order function', () => {
+    it('re reverse array', () => {
+      const arr = [5, 4, 3, 2, 1];
+      expect(inOrder(arr, 'ASC')).toEqual([1, 2, 3, 4, 5]);
+    });
+    it('reverse array', () => {
+      const arr = [1, 2, 3, 4, 5];
+      expect(inOrder(arr, 'DESC')).toEqual([5, 4, 3, 2, 1]);
+    });
+    it('reverse object', () => {
+      const arr = {a: 3, b: 2, c: 1};
+      expect(inOrder(arr, 'ASC')).toEqual({a: 1, b: 2, c: 3});
+    });
+    it('re reverse object', () => {
+      const arr = {a: 1, b: 2, c: 3};
+      expect(inOrder(arr, 'DESC')).toEqual({a: 3, b: 2, c: 1});
+    });
+  })
 })
--- src/calculation/calculation.ts
+++ src/calculation/calculation.ts
@@ -1,3 +1,53 @@
 export const calculation = (a: number, b: number): number => {
   return a + b
 }
+
+export const searchObject = (obj: any, callback: (value: any) => boolean): any => {
+  const result: any = {}
+  const search = (obj: any, path: string) => {
+    for (const key in obj) {
+      const value = obj[key]
+      if (callback(value)) {
+        result[`${path}.${key}`] = value
+      }
+      if (typeof value === 'object') {
+        if (Array.isArray(value)) {
+          value.forEach((item, index) => {
+            search(item, `${path}.${key}[${index}]`)
+          })
+        } else {
+          search(value, `${path}.${key}`)
+        }
+      }
+    }
+  }
+  search(obj, 'object')
+  return result
+}
+
+export const inOrder = (arr: any, order: string): any => {
+  if (Array.isArray(arr) == true) {
+    if (order == "ASC") {
+      return arr.sort((a: any, b: any) => (a < b ? -1 : 1));
+    } else {
+      return arr.sort((a: any, b: any) => (a > b ? -1 : 1));
+    }
+  } else {
+    const sortedValues = Object.values(arr).sort((a: any, b: any) => {
+      if (order === 'ASC') {
+        return a - b;
+      } else if (order === 'DESC') {
+        return b - a;
+      } else {
+        throw new Error('Invalid order. Please use "ASC" or "DESC".');
+      }
+    });
+
+    const sortedObject: any = {};
+    Object.keys(arr).forEach((key, index) => {
+      sortedObject[key] = sortedValues[index];
+    });
+
+    return sortedObject;
+  }
+}
